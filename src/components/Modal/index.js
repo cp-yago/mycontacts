e54165223a3247474abe3d01bf9bc4960b0a84ca
cp-yago <yago.cunha123@gmail.com>
2022-12-06 11:26:05 -0300

    feat: using portal in modal component

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,13 +1,15 @@
+import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import { Overlay, Container, Footer } from './styles';
 import Button from '../Button';
 
 export default function Modal({ danger }) {
-  return (
+  return ReactDOM.createPortal(
     <Overlay>
       <Container danger={danger}>
         <h1>Título do modal</h1>
         <p>Corpo do modal</p>
+
         <Footer>
           <button type="button" className="cancel-button">
             Cancelar
@@ -17,7 +19,8 @@ export default function Modal({ danger }) {
           </Button>
         </Footer>
       </Container>
-    </Overlay>
+    </Overlay>,
+    document.getElementById('modal-root')
   );
 }
 
